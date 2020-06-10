410bffc3bdcca63955332cb62b9c74a3de6e95ff
vigneshp <vigneshp@techaffinity.com>
2020-06-10 17:44:17 +0530

    Updated the paranthesis problem

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-	
+
 
 {
 
-var firstName = 'vignesh'; //can be accessed outside the scope
-let lastName = 'pandian';  //scopped variable
-const notChangable = 1; //scopped variable
+    var firstName = 'vignesh'; //can be accessed outside the scope
+    let lastName = 'pandian';  //scopped variable
+    const notChangable = 1; //scopped variable
 
-	
+
 }
-function TestScope(){
-	console.log(firstName); //will print
-	console.log(lastName); //will raise an error
+function TestScope() {
+    console.log(firstName); //will print
+    console.log(lastName); //will raise an error
 }
 
 //Contructor
-function Phone(name, phoneNumber){
-	this.name = name;
-	this.phoneNumber = phoneNumber;
+function Phone(name, phoneNumber) {
+    this.name = name;
+    this.phoneNumber = phoneNumber;
 }
 
 //prototype for phone
 Phone.prototype = {
-	call: function() { console.log('Calling from phone number ' + this.phoneNumber);}
+    call: function () { console.log('Calling from phone number ' + this.phoneNumber); }
 }
 
 //phone constructor instatiations
@@ -32,10 +32,10 @@ var phone2 = new Phone('Pixel', '9791141980');
 phone1.call = function () { return "hello"; };
 
 //creating array
-var arrayOfElements = ['Vignesh', 'Pandian', 2, function(){ return "vignesh";}]
+var arrayOfElements = ['Vignesh', 'Pandian', 2, function () { return "vignesh"; }]
 
 //for native functions
-Array.prototype.destroyElement =  function(i) { this.splice(0,i);}
+Array.prototype.destroyElement = function (i) { this.splice(0, i); }
 
 
 /*Array Helpers*/
@@ -45,13 +45,13 @@ Array.prototype.destroyElement =  function(i) { this.splice(0,i);}
 var computers = ["Acer", "Dell", "HP", "Orgin PC", undefined];
 
 /*Iteration using forEach*/
-computers.forEach(function(computer){
-	console.log(computer);
+computers.forEach(function (computer) {
+    console.log(computer);
 });
 
-function checkComputerStatus(computer){
-	if(computer!=undefined && computer!=null)
-		console.log("The "+ computer +" is ready to use");
+function checkComputerStatus(computer) {
+    if (computer != undefined && computer != null)
+        console.log("The " + computer + " is ready to use");
 }
 
 /*Passing function reference to forEach*/
@@ -62,14 +62,14 @@ computers.forEach(checkComputerStatus);
 
 /*Map Helper*/
 
-var servers = [{model : 'Acer', ram: 16, isSSD: true, isHighCapacity : true}, {model : 'Dell', ram: 16, isSSD: false, isHighCapacity : true},
-				{model : 'Orgin', ram: 16, isSSD: true, isHighCapacity : false}];
-				
+var servers = [{ model: 'Acer', ram: 16, isSSD: true, isHighCapacity: true }, { model: 'Dell', ram: 16, isSSD: false, isHighCapacity: true },
+{ model: 'Orgin', ram: 16, isSSD: true, isHighCapacity: false }];
+
 
 var serverModels;
 
-serverModels = servers.map(function(server){
-	return  server.model + " server is ready to operate" ;
+serverModels = servers.map(function (server) {
+    return server.model + " server is ready to operate";
 });
 
 serverModels;
@@ -78,43 +78,43 @@ serverModels;
 
 var ssdServers;
 
-ssdServers = servers.filter(function(server){
-	return server.isSSD;
+ssdServers = servers.filter(function (server) {
+    return server.isSSD;
 });
 
 /*complex filter*/
 
 /*Create a function called 'reject'.  Reject should work in the opposite way of 'filter' - if a function returns 'true', the item should *not* be included in the new array.  Hint: you can reuse filter.*/
 
-var numbers = [10,20,30];
+var numbers = [10, 20, 30];
 
 /*Function that returns the opposite value*/
 function reject(array, iteratorFunction) {
-  return array.filter(function(item){
-    return !iteratorFunction(item)
-  });
+    return array.filter(function (item) {
+        return !iteratorFunction(item)
+    });
 }
 
 /*Expected output is 10*/
-var lessThanFifteen = reject(numbers, function(number){
+var lessThanFifteen = reject(numbers, function (number) {
     return number > 15;
 });
 
 /*Find Helper*/
 
 /*find the lessCapacityServer*/
-var lessCapacityServer = servers.find(function(server){
-	return !server.isHighCapacity;
+var lessCapacityServer = servers.find(function (server) {
+    return !server.isHighCapacity;
 });
 
 /*Every and Some helper*/
 
-var doesEveryServerIsHighCapacity = servers.every(function(server){
-	return server.isHighCapacity;
+var doesEveryServerIsHighCapacity = servers.every(function (server) {
+    return server.isHighCapacity;
 });
 
-var anyServersWithHighCapacity = servers.some(function(server){
-	return server.isHighCapacity;
+var anyServersWithHighCapacity = servers.some(function (server) {
+    return server.isHighCapacity;
 });
 
 /*Reduce helper*/
@@ -138,6 +138,22 @@ Case 4: 'vig()' incorrect
 Formula -> (Total no of open brackets - total no of close brackets ) = 0
 
 */
+
+function validateParanthesis(input) {
+    return input.split('').reduce(function (previous, current) {
+
+        //previous less than 0
+        if (previous < 0)
+            return previous;
+
+        if (current === '(')
+            ++previous;
+        if (current === ')')
+            --previous;
+
+        return previous;
+    }, 0);
+}
 
 
 
